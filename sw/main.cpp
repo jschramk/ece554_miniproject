28c4953ce5396a5c3e6cf4d86909c80128eafe56
Jacob Schramkowski <54072198+jschramk@users.noreply.github.com>
2021-09-14 19:33:22 -0500

    Update main.cpp

--- sw/main.cpp
+++ sw/main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
 
       if (result != exp) {
 
-        cerr << "ERROR: Read from MMIO register has incorrect value " << result << " instead of " << exp << endl;
+        cerr << i << ": ERROR: Read from MMIO register has incorrect value " << result << " instead of " << exp << endl;
         errors ++;
 
       }
