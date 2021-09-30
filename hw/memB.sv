fa4343b2e478f0c1b3f0f670f6c93a54a26a0791
Thomas <antonacci@wisc.edu>
2021-09-29 21:24:31 -0500

    replace memb

--- hw/memB.sv
+++ hw/memB.sv
@@ -1,53 +1,35 @@
-// authors: Jacob Schramkowski, Christopher D'Amico, Thomas Antonacci
-
-module memB #( 
-	parameter BITS_AB=8,
-	parameter DIM=8 
-) ( 
-	input clk,rst_n,en, 
-	input signed [BITS_AB-1:0] Bin [DIM-1:0], 
-	output signed [BITS_AB-1:0] Bout [DIM-1:0] 
+module memB #(
+    parameter BITS_AB = 8,
+    parameter DIM = 8
+) (
+    input clk, rst_n, en,
+    input signed [BITS_AB-1:0] Bin [DIM-1:0],
+    output signed [BITS_AB-1:0] Bout [DIM-1:0]
 );
-
-logic [$clog2(DIM):0] count;
-
-genvar col;
+    
+genvar i;
 
 generate
 
-	for (col = 0; col < DIM; col++) begin
+    // this version does not use a counter to feed zeroes,
+    // it relies on the parent module to feed zeroes when it is done feeding B. 
+    // not sure if that is useful or not but I left it in just in case
+    
+    for(i = 0; i < DIM; i++) begin
+        
+        fifo #(
+            .DEPTH(DIM + i),
+            .BITS(BITS_AB)
+        ) FIFO (
+            .clk(clk),
+            .rst_n(rst_n),
+            .en(en),
+            .d(Bin[i]),
+            .q(Bout[i])
+        );
 
-		fifo #(
-			.DEPTH(DIM + col),
-			.BITS(BITS_AB)
-		) FIFO(
-			.clk(clk),
-			.rst_n(rst_n),
-			.en(en),
-			.d(count < DIM ? Bin[col] : {BITS_AB{1'b0}}),
-			.q(Bout[col])
-		);
-
-	end
+    end
 
 endgenerate
-
-// doesn't look like this was being used
-// assign Bout = fifoFeeder[location];
-
-// use a counter to determine when to load zeroes, set to 0 on reset
-always @(posedge clk or negedge rst_n) begin
-
-	if (!rst_n) begin
-
-		count = 0;
-
-	end else if (en) begin
-
-		if (count < DIM) count++;
-
-	end
-
-end
 
 endmodule
