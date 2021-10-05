1a4bd1d1928c9af56033dcec094b851d17a67388
Thomas <antonacci@wisc.edu>
2021-10-05 17:45:02 -0500

    Kalty my boy, argh argh argh argh argh

--- hw/tpuv1.sv
+++ hw/tpuv1.sv
@@ -1,170 +1,166 @@
-module tpuv1 #(
+module tpuv1
+  #(
     parameter BITS_AB=8,
     parameter BITS_C=16,
     parameter DIM=8,
     parameter ADDRW=16,
     parameter DATAW=64
-) (
+    )
+   (
     input clk, rst_n, r_w, // r_w=0 read, =1 write
     input [DATAW-1:0] dataIn,
     output [DATAW-1:0] dataOut,
     input [ADDRW-1:0] addr
-);
+   );
+   
 
-logic Aen, Ben, SAen, AWrEn, SAWrEn, high;
-logic [$clog2(DIM)-1:0] Arow;
-logic [$clog2(DIM)-1:0] Crow;
-logic signed [BITS_AB-1:0] Aout [DIM-1:0];
-logic signed [BITS_AB-1:0] Bout [DIM-1:0];
-logic [$clog2(3*DIM-2):0] count;
-logic signed [BITS_AB-1:0] ABDataIn [DIM-1:0];
-logic signed [BITS_AB-1:0] Bin [DIM-1:0];
-logic signed [BITS_C-1:0] CDataIn [DIM-1:0];
-logic signed [BITS_C-1:0] CDataOut [DIM-1:0];
-logic signed [BITS_C-1:0] CHalf [(DIM/2) - 1:0];
-logic signed [DATAW*2-1:0] COutRaw;
-
-
-genvar i;
-generate
-
-	for (i = 0; i < DIM; i++)  begin
-		assign ABDataIn[i] = dataIn[8*i+7:i*8];
-		assign COutRaw[16*i + 15: 16*i] = CDataOut[i];
-		assign Bin[i] = (count > 0) ? {DIM{1'b0}} : ABDataIn[i];
-	end
-
-	for (i = 0; i < DIM/2; i++) begin
-		assign CHalf[i] = dataIn[16*i + 15: 16*i];
-	end
-
-endgenerate
-
-assign AWrEn = (count == 0 || count > 3*DIM-2) && r_w && (addr >= 16'h100 && addr <= 16'h13f);
-assign Arow = addr[5:3];
-assign Ben = ((count == 0 || count > 3*DIM-2) && r_w && (addr >= 16'h200 && addr <= 16'h23f)) || (count > 0) || addr >= 16'h400;
-assign Crow = addr[6:4];
-assign high = addr[3];
-assign SAWrEn = (count == 0 || count > 3*DIM-2) && r_w && (addr >= 16'h300 && addr <= 16'h37f);
-assign CDataIn = high ? {CHalf, CDataOut[3:0]} : {CDataOut[7:4], CHalf};
-assign dataOut = high ? COutRaw[127: 64] : COutRaw[63:0];
-assign Aen = (count > 0) || addr >= 16'h400;
-assign SAen = (count > 0 && count <= 3*DIM-2) || (r_w && addr == 16'h0400) || (!r_w && addr >= 16'h300 && addr <= 16'h37f);
-
-memA #(
-    .BITS_AB(BITS_AB),
-    .DIM(DIM)
-) MEMA (
-    .clk(clk),
-    .rst_n(rst_n),
-    .en(Aen),
-    .WrEn(AWrEn), 
-	.Ain(ABDataIn), 
-	.Arow(Arow), 
-	.Aout(Aout)
-);
-
-memB #(
-    .BITS_AB(BITS_AB),
-    .DIM(DIM)
-) MEMB (
-    .clk(clk),
-    .rst_n(rst_n),
-    .en(Ben),
-	.Bin(Bin), 
-	.Bout(Bout) 
-);
-
-systolic_array #(
-    .BITS_AB(BITS_AB),
-    .BITS_C(BITS_C),
-    .DIM(DIM)
-) SYS_ARR (
-    .clk(clk),
-    .rst_n(rst_n),
-    .WrEn(SAWrEn),
-    .en(SAen),
-    .A(Aout),
-    .B(Bout),
-    .Cin(CDataIn),
-    .Crow(Crow),
-    .Cout(CDataOut)
-);
-
-always @(posedge clk or negedge rst_n) begin
-	if (!rst_n) begin
-		count <= 0;
-	end else if (count > 3*DIM-2) begin
-		count <= 0;
-	end else if (addr == 16'h0400 || count > 0) begin
-		count++;
-	end
-end
-
-/*
-always @(posedge clk or negedge rst_n) begin
-
-	if (!rst_n) begin
-		count <= 0;
-	end
-
-	Aen <= 0;
-	Ben <= 0;
-	SAen <= 0;
-	AWrEn <= 0;
-	SAWrEn <= 0;
-	high <= 0;
-
-	if (count == 0 || count > 3*DIM-2) begin
-		if(r_w) begin // on write, load data based on addr
-			
-			if(addr >= 16'h100 && addr <= 16'h13f) begin
-				
-				// write into A
-				Arow <= addr[7:0] / 8;
-				AWrEn <= 1;
-				count <= 0;
-
-			end else if (addr >= 16'h200 && addr <= 16'h23f) begin
-				
-				// write into B
-				Ben <= 1;
-				count <= 0;
-
-			end else if (addr >= 16'h300 && addr <= 16'h37f) begin
-				
-				// write into C
-				Crow <= addr[6:4];
-				high <= addr[3];
-				SAWrEn <= 1;
-				count <= 0;
-				
-				CDataIn <= high ? {CHalf, CDataOut[3:0]} : {CDataOut[7:4], CHalf};
-
-			end else if(addr == 16'h400) begin
-				
-				// start computation
-				count++;
-				SAen <= 1;
-				
+	// Interconnects
+	wire signed [BITS_AB-1:0] memAInput[DIM-1:0];
+	wire signed [BITS_AB-1:0] memBInput[DIM-1:0];
+	
+	wire signed [BITS_AB-1:0] memAOut[DIM-1:0];
+	wire signed [BITS_AB-1:0] memBOut[DIM-1:0];
+	
+	// Store for high low C outs read/writes
+	wire signed [BITS_C-1:0] macCIn[DIM-1:0];
+	wire signed [BITS_C-1:0] macCOut[DIM-1:0];
+	
+	logic [DATAW-1:0] readC;
+	assign dataOut = readC;
+	
+	// Regs for counter
+	reg [($clog2(4*DIM))-1:0] counter;
+	
+	// Control sigs
+	logic write_a, write_b, write_c, en, start;
+	
+	// Row Sel
+	wire [($clog2(DIM))-1:0] aRowSel;
+	wire [($clog2(DIM))-1:0] cRowSel;
+	assign aRowSel = addr[5:3];
+	assign cRowSel = addr[6:4];
+	
+	//----------------------------------------------------------------------------------------
+	
+	// Connecting Interconnets to inputs
+	// A,B
+	genvar i;
+	generate
+		for (i = 1; i < DIM+1; i++) begin
+			assign memAInput[i-1] = dataIn[(i*BITS_AB)-1:((i-1)*BITS_AB)];
+			assign memBInput[i-1] = (en) ? (0) : (dataIn[(i*BITS_AB)-1:((i-1)*BITS_AB)]); // fill zeros
+		end
+	endgenerate
+	
+	// C
+	generate
+		for (i = 1; i < DIM+1; i++) begin
+			if (i < (DIM/2)+1) begin // low case
+				assign macCIn[i-1] = (addr[3:0] == 'h0) ? (dataIn[(i*BITS_C)-1:(i-1)*BITS_C]) : (macCOut[i-1]);
 			end
-
-		end else begin
-			if (addr >= 16'h300 && addr <= 16'h37f) begin
-				
-				// read from C
-				Crow <= addr[6:4];
-				high <= addr[3];
-				SAen <= 1;
-				dataOut <= high ? COutRaw[127: 64] : COutRaw[63:0];
-				
+			else begin // high case
+				assign macCIn[i-1]  = (addr[3:0] == 'h8) ? (dataIn[((i-DIM/2)*BITS_C)-1:((i-DIM/2)-1)*BITS_C]) : (macCOut[i-1]);
 			end
 		end
-	end else begin
-		count++;
-		SAen <= 1;
+	endgenerate
+	
+	
+	// Modules
+	memA #(.BITS_AB(BITS_AB), .DIM(DIM)) iMEMA(
+		.clk(clk),
+		.rst_n(rst_n),
+		.en(en),
+		.WrEn(write_a),
+		.Ain(memAInput),
+		.Arow(aRowSel),
+		.Aout(memAOut)
+		);
+		
+	memB #(.BITS_AB(BITS_AB), .DIM(DIM)) iMEMB(
+		.clk(clk),
+		.rst_n(rst_n),
+		.en(en | write_b),
+		.Bin(memBInput),
+		.Bout(memBOut)
+		);
+	
+	systolic_array #(.BITS_AB(BITS_AB), .BITS_C(BITS_C), .DIM(DIM)) iSYSARRY(
+		.clk(clk),
+		.rst_n(rst_n),
+		.WrEn(write_c),
+		.en(en),
+		.A(memAOut),
+		.B(memBOut),
+		.Cin(macCIn),
+		.Crow(cRowSel),
+		.Cout(macCOut)
+		);
+	
+	
+	// Control gen
+	always_comb begin
+		// Defaults
+		write_a = 0;
+		write_b = 0;
+		write_c = 0;
+		start = 0;
+		readC = 0;
+		
+		// Cases from addrs lines
+		casex(addr)
+			// Write A
+			'h1xx:
+				if (r_w) begin
+					write_a = 1;
+				end
+			
+			// Write BITS_AB
+			'h2xx:
+				if (r_w) begin
+					write_b = 1;
+				end
+				
+			// Read C High word
+			'h3x8:
+				if(r_w) begin
+					write_c = 1;
+				end
+				else begin
+					for(integer x = DIM/2; x < DIM; x++) begin
+						readC |= (macCOut[x] << ((x-DIM/2)*BITS_C));
+					end
+				end
+			
+			// Read C Low word
+			'h3x0:
+				if(r_w) begin
+					write_c = 1;
+				end
+				else begin
+					for(integer x = 0; x < DIM/2; x++) begin
+						readC |= (macCOut[x] << (x*BITS_C));
+					end
+				end
+			
+			// Start
+			'h400:
+			if (r_w) begin
+				start = 1; // Starts counter
+			end
+		endcase
+	
 	end
-end*/
-
-
+	
+	
+	// Counter/en sig
+	assign en = |counter;
+	always_ff @(posedge clk) begin
+		if (~rst_n) begin
+			counter <= 0;
+		end	
+		else if (start || en) begin
+			counter <= counter+1;
+		end
+	end
+   
 endmodule
